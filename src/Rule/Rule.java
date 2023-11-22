50840179eaff744661e8a1dc2927a98ae912daf8
SimoneBit <148981358+SimoneBit@users.noreply.github.com>
2023-11-22 16:16:53 +0100

    Implementation Rule and Trigger

--- src/Rule/Rule.java
+++ src/Rule/Rule.java
@@ -4,10 +4,43 @@
  */
 package Rule;
 
+import Action.Action;
+import Condition.Trigger;
+
 /**
  *
  * @author Simone
  */
 public class Rule {
+    private String name;
+    private Trigger trigger;
+    private Action action;
+    private boolean Active;
+
+    public Rule(String name, Trigger trigger, Action action) {
+        this.name = name;
+        this.trigger = trigger;
+        this.action = action;
+        this.Active = true;
+    }
+
+    
+    public boolean checkRule(){
+        boolean exit = false;
+        if(trigger.checkTrigger() == true){
+            action.executeAction();
+            exit = true;
+        }        
+        return exit;
+    }
+    
+    public boolean isActive() {
+        return Active;
+    }
+
+    public void setActive(boolean Active) {
+        this.Active = Active;
+    }
+    
     
 }
