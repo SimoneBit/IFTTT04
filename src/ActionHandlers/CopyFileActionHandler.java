ff6bb7c33d4d1632a0c50efd4729a5e8d68eb473
palma-o <p.orlando8@studenti.unisa.it>
2023-11-30 10:03:27 +0100

    Test file inseriti

--- src/ActionHandlers/CopyFileActionHandler.java
+++ src/ActionHandlers/CopyFileActionHandler.java
@@ -1,9 +1,16 @@
 package ActionHandlers;
 
+import Action.Action;
+
 /**
  *
  * @author Palma Orlando
  */
-public class CopyFileActionHandler {
+public class CopyFileActionHandler extends BaseActionHandler{
+
+    @Override
+    public Action handle(String request, String param) {
+        return super.handle(request, param); //To change body of generated methods, choose Tools | Templates.
+    }
     
 }
